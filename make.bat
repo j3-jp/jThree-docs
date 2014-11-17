69d262b2a3d166f86d70aefe498f7a9b2f9024e3
m2wasabi <m2wasabi@gmail.com>
2014-11-18 03:09:31 +0900

    Update make commands

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 for %%i in (src\*.md) do (
 echo %%~ni
-pandoc %%i -f markdown_github+definition_lists -t html5 --template=template\pandoc_template.html --toc --toc-depth=4 -s -o html\%%~ni.html
+pandoc %%i -f markdown_github+definition_lists -t html5 --template=template\pandoc_template.html -c css/normalize.css -c css/doc.css -c css/common.css --toc --toc-depth=6 -s -o html\%%~ni.html
 )
